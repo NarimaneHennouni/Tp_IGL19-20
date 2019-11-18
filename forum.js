ab7997ae0b2c4573b461108b9ed902fdee29e6a8
Nour EL HASSANE <hn_el_hassane@esi.dz>
2019-11-19 00:58:56 +0100

    servicPoserQuestionForum

---
+++ forum.js
@@ -0,0 +1,52 @@
+const mysql=require('mysql');
+const express=require('express');
+var app=express();
+const bodyparser=require('body-parser');
+
+app.use(bodyparser.json());
+var mysqlConnection=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'forumesi',
+    multipleStatements:true,
+    
+});
+
+mysqlConnection.connect((err)=>{
+    if(!err) console.log('DB connection succeded');
+    else console.log('DB connection failed \n Error : '+JSON.stringify(err,undefined,2));
+});
+
+app.listen(3000,()=>console.log('Express server is running at port no : 3000'));
+//récuperer les questions depuis la BDD
+app.get('/forumesi',(req,res)=>{
+    mysqlConnection.query('SELECT * FROM questions',(err,rows,fields)=>{
+        if(!err) {res.send(rows); //transform the response into a JSON format
+        console.log(rows[0].question);}
+        else console.log(err);
+    })
+});
+
+//récuperer une question dont l'id est égale à 'id' depuis la BDD
+app.get('/forumesi/:id',(req,res)=>{
+    mysqlConnection.query('SELECT * FROM questions WHERE id_question = ?',[req.params.id],(err,rows,fields)=>{
+        if(!err) res.send(rows); //transform the response into a JSON format
+        else console.log(err);
+    })
+});
+
+//insérer une question dans la BDD
+app.post('/forumesi',(req,res)=>{
+    let ques=req.body;
+    var sql="SET @id_question = ?;SET @id_user = ?;SET @question = ?;\
+    CALL poserquestion(@id_question,@id_user,@question);";
+    mysqlConnection.query(sql,[ques.id_question,ques.id_user,ques.question],(err,rows,fields)=>{
+        if(!err)
+        rows.forEach(element => {
+            if(element.constructor == Array)
+            res.send('inserted question id :'+element[0].id_question);
+        });
+        else console.log(err);
+    })
+});
