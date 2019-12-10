10d158536e9440569bb60d355c3299efff792d1d
narimane00 <hn_hennouni@esi.dz>
2019-12-10 12:43:56 +0100

    works perfectly!!!

--- essai.js
+++ essai.js
@@ -1,6 +1,6 @@
 var mysql      = require('mysql');
 var http       = require('http');
-var coeff,notes,moys;
+var coeff,moys,nom_mod;
 var i=0;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
@@ -21,20 +21,8 @@ connection.connect(function(err) {
 
     console.log('Connected as id ' + connection.threadId);
 });
-function remplir(notes,i)
-{
-  var k=0
-  var mysql3 = "UPDATE "+module[i]+" SET MOY=? WHERE id_etud = ?";
-           while(notes[k]!=null){
-            connection.query(mysql3,[moy[k],notes[k].id_etud] , function (error, results) {
-            if (error)
-            throw error;
-            console.log("inserted");
 
-        });
-        k++; 
-      } 
-}
+
 
 var modules=[];
 module[0]='notes_anum';
@@ -42,55 +30,47 @@ module[1]="notes_igl";
 module[2]="notes_res";
 module[3]="notes_thp";
 var sql1="SELECT CC, CI, TP, CF FROM modules WHERE id_module=?";
-
-i=0;
-for (i; i<4;i++)
+async function remplir(nom_mod,i)
 {
- var l=0;
-  console.log(i);
-   connection.query(sql1,[i+1], function (error, results2, fields){
+   var k= await connection.query(sql1,[i], function (error, results2, fields){
     if (error)
     throw error;
     coeff=results2;
-    console.log("premier i");
-    console.log(i);
   })
-  console.log(" le entre i");
-  console.log(i);
-   connection.query("SELECT id_etud, CC, CI, TP, CF FROM "+module[i], function (error, results1, fields) {
+   connection.query("SELECT id_etud, CC, CI, TP, CF FROM "+nom_mod, function (error, results1, fields) {
     if (error)
         throw error;
-        console.log("deuxieme query");
         console.log(results1);
-        console.log("le éeme i");
-        console.log(i);
-        var notes1=results1;
-        notes=notes1;
        results1.forEach (result => {
           var somme_coeff=coeff[0].CC+coeff[0].CI+coeff[0].TP+coeff[0].CF;
           var moy=((result.CC * coeff[0].CC+result.CI * coeff[0].CI+result.TP * coeff[0].TP+result.CF * coeff[0].CF)/(somme_coeff));
           var j=0;
           console.log('la moyenne est');
           console.log(moy);
-          moys[j]=moy;
-          
-          j++;
+          var mysql3 = "UPDATE "+nom_mod+" SET MOY=? WHERE id_etud = ?";
+
+          connection.query(mysql3,[moy,result.id_etud] , function (error, results) {
+          if (error)
+          throw error;
+          console.log("inserted");
+
+      });  
         });
-      }).then(remplir(notes,i))
-          //console.log(i);
-         // console.log(module[i]);
-         /*  var mysql3 = "UPDATE "+module[i]+" SET MOY=? WHERE id_etud = ?";
-
-            connection.query(mysql3,[moy,result.id_etud] , function (error, results) {
-            if (error)
-            throw error;
-            console.log("inserted");
-
-        });  
       
-});*/
-//console.log("ici les notes");
-//console.log(notes[1]);
+      
+});
+}
+
+for(i=0;i<4;i++){
+  console.log("am in");
+  nom_mod=module[i];
+  console.log(nom_mod);
+  console.log("le i");
+  console.log(i);
+  var y= remplir(nom_mod,i);
+  console.log("apres");
+  console.log(i);
+}
 
 
 
@@ -114,4 +94,3 @@ for (i; i<4;i++)
 
 
 //connection.end();
-}
