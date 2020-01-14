2586a006c2869ec320cec393b59d74413597f679
Nour EL HASSANE <hn_el_hassane@esi.dz>
2020-01-14 20:39:58 +0100

    maintenance

--- server.js
+++ server.js
@@ -16,21 +16,13 @@ const express = require('express');
    * @member port 
    * @description specified port for the backend (3000)
    */
-  //port = process.env.PORT || 3000;
+  port = process.env.PORT || 3000;
 
 const mysql = require('mysql');
 /**
  * database's connection configurations
  * @type {Object}
  */
-
- var connection = mysql.createConnection({
-  host            : process.env.DATABASE_HOST,
-  port            : process.env.MYSQL_PORT,
-  user            : process.env.MYSQL_USER,
-  password        : process.env.MYSQL_PASSWORD,
-  database        : process.env.MYSQL_DATABASE
-});
 
 var connection = mysql.createConnection({
   host            : 'localhost',
@@ -39,16 +31,9 @@ var connection = mysql.createConnection({
   database        : 'forumesi'
 });
 
-//app.listen(3003,()=>console.log("console satrted"));
+app.listen(port);
 
- connection.connect(function(err) {
-  if (err) {
-      console.error('Error connecting: ' + err.stack);
-      return;
-  }
-
-  console.log('Connected as id ' + connection.threadId);
-});
+ connection.connect();
 
 
 //console.log('API server started on: ' + port);
--- unit-tests/service.test.js
+++ unit-tests/service.test.js
@@ -26,7 +26,9 @@ test('server should return questions',async ()=>
 {
     expect.assertions(1);
     const data=await fetchQuestions();
-    expect(data).toEqual([{"id_qst":1,"id_user":5,"question":"c'est quoi docker ?"},{"id_qst":2,"id_user":8,"question":"à quoi sert selenium ?"},{"id_qst":3,"id_user":4,"question":"est-ce que la méthodologie scrum est une méthode agile ?"},{"id_qst":4,"id_user":14,"question":"peut-on utiliser mocha pour le unit testing ?"}]);
+    expect(data).toEqual([{"id_qst":1,"id_user":5,"question":"c'est quoi docker ?"},{"id_qst":2,"id_user":8,"question":"à quoi sert selenium ?"},
+    {"id_qst":3,"id_user":4,"question":"est-ce que la méthodologie scrum est une méthode agile ?"},{"id_qst":4,"id_user":14,"question":"peut-on utiliser mocha pour le unit testing ?"},
+    {"id_qst":5,"id_user":0,"question":"is jsdoc used for automatic documentation ?"}]);
     
 });
 /**
